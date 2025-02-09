2afe594f28cd7dba9ecf3afa8801d187a06f7104
mowglixx <mowglixx0@gmail.com>
2025-02-09 20:22:58 +0000

    stopped unused imports from causing the build to fail

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript")
+  ...compat.config(
+    {
+      extends: [
+        "next",
+        "next/core-web-vitals",
+        "next/typescript"
+      ],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    })
 ];
 
 export default eslintConfig;
