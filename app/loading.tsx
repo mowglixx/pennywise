c5e38f6e69aaa35a60e0c0df0fcb52dddc12a835
Dan Monaghan <bionicbeaver@tuta.io>
2025-02-16 16:56:50 +0000

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from "@chakra-ui/react"
+
+
+// I'm not sure why this doesn't work, not a huge issue just a pain
+const Loading = () => {
+    return (
+        <Spinner />
+    )
+}
+export default Loading
