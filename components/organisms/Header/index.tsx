af4f5e7cb9d82a2957a483c30e30d9b89342c14e
Dan Monaghan <bionicbeaver@tuta.io>
2025-02-16 17:09:07 +0000

    Impoved navbar order
    moved to a useMemo to improve performance

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -14,54 +14,66 @@ const Header = () => {
 
   const { data } = useSession()
 
-  const navItems = []
-
-  if (data?.user) {
-    navItems.push(
+  const navItems = useMemo(() => {
+    const items = [
       {
         id: 'home',
-        href: '/manage/dash',
-        label: 'Dashboard',
-        description: 'See insights about your budget'
+        href: '/',
+        label: 'Home',
+        description: 'Back to the homepage'
       },
       {
-        id: 'income',
-        href: '/manage/incomes',
-        label: 'Incomes',
-        description: 'Keep track of your income.'
+        id: 'help',
+        href: '/help',
+        label: 'Help',
+        description: 'Docs and FAQ'
       },
-      {
-        id: 'expense',
-        href: '/manage/expenses',
-        label: 'Expenses',
-        description: 'Track One-off expenses.'
-      },
-      {
-        id: 'food_shopping',
-        href: '/manage/expenses/food',
-        label: 'Food Budget',
-        description: 'Manage your food Budget'
-      },
-      {
-        id: 'bill',
-        href: '/manage/expenses/bills',
-        label: 'Bills',
-        description: 'Keep track of bills'
-      });
-  }
+    ]
 
-  navItems.push({
-    id: 'help',
-    href: '/help',
-    label: 'Help',
-    description: 'Docs and FAQ'
-  })
+
+    if (data?.user) {
+      items.unshift(
+        {
+          id: 'dash',
+          href: '/manage/dash',
+          label: 'Dashboard',
+          description: 'See insights about your budget'
+        },
+        {
+          id: 'income',
+          href: '/manage/incomes',
+          label: 'Incomes',
+          description: 'Keep track of your income.'
+        },
+        {
+          id: 'expense',
+          href: '/manage/expenses',
+          label: 'Expenses',
+          description: 'Track One-off expenses.'
+        },
+        {
+          id: 'food_shopping',
+          href: '/manage/expenses/food',
+          label: 'Food Budget',
+          description: 'Manage your food Budget'
+        },
+        {
+          id: 'bill',
+          href: '/manage/expenses/bills',
+          label: 'Bills',
+          description: 'Keep track of bills'
+        });
+    }
+    return items
+  }, [data])
+
+
   return (
-    <Stack p={'5'} position={'fixed'} width={'100%'} zIndex={999} outline={'solid'} backdropFilter={'blur(3px) brightness(0.25)'}>
+    <Stack p={'5'} position={'fixed'} width={'100%'} zIndex={999} outline={'solid'} backdropFilter={'blur(10px)'}>
       <HStack justifyContent={'space-between'}>
         <LinkBox>
           <LinkOverlay asChild>
-            <Link aria-label="Homepage" href="/">
+            <Link aria-label="Homepage" href={data?.user ? "/manage/dash" : "/"}>
               <LuBanknote />
               <Heading as={'h1'}>
                 Pennywise
