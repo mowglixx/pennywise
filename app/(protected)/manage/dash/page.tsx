cf5183def43fb56b6da23dee5bc825d5c69e6472
Dan Monaghan <bionicbeaver@tuta.io>
2025-02-20 02:57:04 +0000

    added chart to dash with filtered data

--- app/(protected)/manage/dash/page.tsx
+++ app/(protected)/manage/dash/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { UserDataContext } from "@/components/contexts/UserDataProvider"
-import { Heading, Stack } from "@chakra-ui/react"
-import { useContext } from "react"
+import { DataList, DataListItem, DataListItemLabel, DataListItemValue, DataListRoot, Grid, GridItem, Heading, Stack } from "@chakra-ui/react"
+import { useContext, useMemo } from "react"
 import 'chart.js/auto';
 import { Chart } from "react-chartjs-2"
 import { Prisma } from "@prisma/client";
@@ -12,26 +12,68 @@ import { Prisma } from "@prisma/client";
 const DashboardPage = () => {
     const { userData } = useContext(UserDataContext)
 
+    const chartDataSet = useMemo(() => [
+        {
+            label: "Income",
+            data: userData.incomes.map((income: Prisma.IncomeCreateWithoutUserInput) => Number(income.amount)).reduce((a, b) => a + b, 0)
+        },
+        {
+            label: "Expenses",
+            data: userData.expenses.map((expense: Prisma.ExpenseCreateWithoutUserInput) => Number(expense.amount)).reduce((a, b) => a + b, 0)
+        },
+        {
+            label: "Bills",
+            data: userData.bills.map((bill: Prisma.BillCreateWithoutUserInput) => Number(bill.amount)).reduce((a, b) => a + b, 0)
+        },
+        {
+            label: "Shopping",
+            data: userData.shopping.map((shopping: Prisma.ShoppingCreateWithoutUserInput) => Number(shopping.amount)).reduce((a, b) => a + b, 0)
+        },
+    ].filter(o => o.data !== 0), [userData?.incomes, userData?.bills, userData?.expenses, userData?.shopping,]
+    )
+    // remove irrelevant data
+
     return (
-        <Stack>
-            <Heading>
-                Dashboard
-            </Heading>
-            {userData?.incomes?.map && (
-                <Chart
-                    type="doughnut"
-                    title="Incomes"
-                    data={{
-                        labels: [...userData.incomes.map((income: Prisma.IncomeCreateWithoutUserInput) => income.source)],
-                        datasets: [{
-                            label: 'Amount',
-                            data: userData.incomes.map((income: Prisma.IncomeCreateWithoutUserInput) => Number(income.amount).toFixed(2)),
-                        }]
-                    }}
-                    redraw
-                />)
-            }
-        </Stack>
+        <>
+            <GridItem>
+                <Stack>
+                    <Heading>
+                        Dashboard
+                    </Heading>
+
+                    {chartDataSet && (
+                        <Chart
+                            type="doughnut"
+                            data={{
+                                labels: chartDataSet.map(o => o.label),
+                                datasets: [{
+                                    label: 'Amount',
+                                    data: chartDataSet.map(o => o.data.toFixed(2)),
+                                }]
+                            }}
+                            options={{
+                                responsive: true
+                            }}
+                            redraw
+                        />)
+                    }
+                </Stack>
+            </GridItem>
+
+            <GridItem>
+                <Stack>
+                    <Heading>
+                        Summary
+                    </Heading>
+                    <DataListRoot>
+                        <DataListItem >
+                            <DataListItemLabel>a</DataListItemLabel>
+                            <DataListItemValue>b</DataListItemValue>
+                        </DataListItem>
+                    </DataListRoot>
+                </Stack>
+            </GridItem>
+        </>
     )
 }
 export default DashboardPage
